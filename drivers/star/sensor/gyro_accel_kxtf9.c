caaa24219178368d0a9c1c46e4a674b0d2f24f88
vadonka <vadonka@vadonka.com>
2012-02-18 22:43:52 +0100

    gyro accel fix

--- drivers/star/sensor/gyro_accel_kxtf9.c
+++ drivers/star/sensor/gyro_accel_kxtf9.c
@@ -1322,20 +1322,16 @@ static int star_accel_remove( struct platform_device *pdev )
 	return 0;
 }
 
-extern int star_proxi_get_status(void);
-
 int star_accel_suspend(struct platform_device *dev, pm_message_t state)
 {
-    if (!star_proxi_get_status())
-        star_accel_disable_irq();
+	//star_accel_set_power_rail(g_accel->vdd_id, NV_FALSE);
 
 	return 0;
 }
 
 int star_accel_resume(struct platform_device *dev)
 {
-    if (!star_proxi_get_status())
-        star_accel_enable_irq();
+	//star_accel_set_power_rail(g_accel->vdd_id, NV_TRUE);
 
 	return 0;
 }
