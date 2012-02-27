a4ca3f1378d128269be7c20c23275b00a45d808b
vadonka <vadonka@vadonka.com>
2012-02-27 17:39:05 +0100

    Increase CPU low corners for better stability

--- arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_power_dfs.h
+++ arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_power_dfs.h
@@ -280,10 +280,10 @@ extern "C"
 #define NVRM_DTT_POLL_MS_SLOW           (8000UL)
 
 /// Default low corners for core and dedicated CPU voltages
-#define NVRM_AP20_LOW_CORE_MV (900)
+#define NVRM_AP20_LOW_CORE_MV (950)
 #define NVRM_AP20_LOW_CPU_MV (750)
 /// Core voltage in suspend
-#define NVRM_AP20_SUSPEND_CORE_MV (950)
+#define NVRM_AP20_SUSPEND_CORE_MV (1000)
 
 /// Core and CPU voltage reliability requirements for some skus
 #define NVRM_AP20_RELIABILITY_CORE_MV(sku) \
