e56aa5c644b2255789c20ecaf381cdbbb2636063
vadonka <vadonka@vadonka.com>
2012-03-05 23:43:07 +0100

    Minor visual patch

--- drivers/misc/blx.c
+++ drivers/misc/blx.c
@@ -18,88 +18,87 @@ static int charging_limit = MAX_CHARGINGLIMIT;
 
 static ssize_t blx_charginglimit_read(struct device * dev, struct device_attribute * attr, char * buf)
 {
-    return sprintf(buf, "%u\n", charging_limit);
+	return sprintf(buf, "%u\n", charging_limit);
 }
 
 static ssize_t blx_charginglimit_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
 {
-    unsigned int data;
+	unsigned int data;
 
-    if(sscanf(buf, "%u\n", &data) == 1) 
+	if(sscanf(buf, "%u\n", &data) == 1) 
 	{
-	    if (data >= 0 && data <= MAX_CHARGINGLIMIT)
+		if (data >= 0 && data <= MAX_CHARGINGLIMIT)
 		{
-		    charging_limit = data;
-		    
-		    pr_info("BLX charging limit set to %u\n", charging_limit);
+			charging_limit = data;
+			pr_info("BLX charging limit set to %u\n", charging_limit);
 		}
-	    else
+		else
 		{
-		    pr_info("%s: invalid input range %u\n", __FUNCTION__, data);
+			pr_info("%s: invalid input range %u\n", __FUNCTION__, data);
 		}
-	} 
-    else 
+	}
+	else
 	{
-	    pr_info("%s: invalid input\n", __FUNCTION__);
+		pr_info("%s: invalid input\n", __FUNCTION__);
 	}
 
-    return size;
+	return size;
 }
 
 static ssize_t blx_version(struct device * dev, struct device_attribute * attr, char * buf)
 {
-    return sprintf(buf, "%u\n", BATTERYLIFEEXTENDER_VERSION);
+	return sprintf(buf, "%u\n", BATTERYLIFEEXTENDER_VERSION);
 }
 
 static DEVICE_ATTR(charging_limit, S_IRUGO | S_IWUGO, blx_charginglimit_read, blx_charginglimit_write);
 static DEVICE_ATTR(version, S_IRUGO , blx_version, NULL);
 
-static struct attribute *blx_attributes[] = 
-    {
+static struct attribute *blx_attributes[] =
+{
 	&dev_attr_charging_limit.attr,
 	&dev_attr_version.attr,
 	NULL
-    };
+};
 
-static struct attribute_group blx_group = 
-    {
+static struct attribute_group blx_group =
+{
 	.attrs  = blx_attributes,
-    };
+};
 
-static struct miscdevice blx_device = 
-    {
+static struct miscdevice blx_device =
+{
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = "batterylifeextender",
-    };
+};
 
 int get_charginglimit(void)
 {
-    return charging_limit;
+	return charging_limit;
 }
 EXPORT_SYMBOL(get_charginglimit);
 
 static int __init blx_init(void)
 {
-    int ret;
+	int ret;
 
-    pr_info("%s misc_register(%s)\n", __FUNCTION__, blx_device.name);
+	pr_info("%s misc_register(%s)\n", __FUNCTION__, blx_device.name);
 
-    ret = misc_register(&blx_device);
+	ret = misc_register(&blx_device);
 
-    if (ret) 
+	if (ret)
 	{
-	    pr_err("%s misc_register(%s) fail\n", __FUNCTION__, blx_device.name);
+		pr_err("%s misc_register(%s) fail\n", __FUNCTION__, blx_device.name);
 
-	    return 1;
+		return 1;
 	}
 
-    if (sysfs_create_group(&blx_device.this_device->kobj, &blx_group) < 0) 
+	if (sysfs_create_group(&blx_device.this_device->kobj, &blx_group) < 0) 
 	{
-	    pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
-	    pr_err("Failed to create sysfs group for device (%s)!\n", blx_device.name);
+		pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
+		pr_err("Failed to create sysfs group for device (%s)!\n", blx_device.name);
 	}
 
-    return 0;
+	return 0;
 }
 
 device_initcall(blx_init);
