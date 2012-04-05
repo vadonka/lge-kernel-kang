8b6e9bf1d7c6e3ce6a74924488c4b5f5eac43b0d
vadonka <vadonka@vadonka.com>
2012-04-05 18:41:17 +0200

    Use arm-linux-androideabi cross compiler

--- compile.sh
+++ compile.sh
@@ -18,8 +18,8 @@ export kinstsrc=/home/android/android/kernel-installer/source
 export mthd=`grep 'processor' /proc/cpuinfo | wc -l`
 export mthm=1
 # Compiler
-export cc=arm-linux-gnueabi-
-export cc=/home/android/android/android-toolchain-eabi-4.6.4/bin/arm-eabi-
+export cc=arm-linux-androideabi-
+export cc=/home/android/android/android-toolchain-eabi-4.6.4/bin/arm-linux-androideabi-
 ######################################################
 
 # Check executables
