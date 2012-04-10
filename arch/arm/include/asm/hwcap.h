b499cd9b5378b9d389e947b2127b7a3860681246
Peter Donka <vadonka@vadonka.com>
2012-04-10 08:58:58 +0200

    Fix BPF_JIT build error on kernel 3.0.y

--- arch/arm/include/asm/hwcap.h
+++ arch/arm/include/asm/hwcap.h
@@ -4,22 +4,26 @@
 /*
  * HWCAP flags - for elf_hwcap (in kernel) and AT_HWCAP
  */
-#define HWCAP_SWP	1
-#define HWCAP_HALF	2
-#define HWCAP_THUMB	4
-#define HWCAP_26BIT	8	/* Play it safe */
+#define HWCAP_SWP		1
+#define HWCAP_HALF		2
+#define HWCAP_THUMB		4
+#define HWCAP_26BIT		8	/* Play it safe */
 #define HWCAP_FAST_MULT	16
-#define HWCAP_FPA	32
-#define HWCAP_VFP	64
-#define HWCAP_EDSP	128
-#define HWCAP_JAVA	256
+#define HWCAP_FPA		32
+#define HWCAP_VFP		64
+#define HWCAP_EDSP		128
+#define HWCAP_JAVA		256
 #define HWCAP_IWMMXT	512
 #define HWCAP_CRUNCH	1024
 #define HWCAP_THUMBEE	2048
-#define HWCAP_NEON	4096
-#define HWCAP_VFPv3	8192
+#define HWCAP_NEON		4096
+#define HWCAP_VFPv3		8192
 #define HWCAP_VFPv3D16	16384
-#define HWCAP_TLS	32768
+#define HWCAP_TLS		32768
+#define HWCAP_VFPv4		65536
+#define HWCAP_IDIVA		131072
+#define HWCAP_IDIVT		262144
+#define HWCAP_IDIV		524288
 
 #if defined(__KERNEL__) && !defined(__ASSEMBLY__)
 /*
