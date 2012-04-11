e6ae3d87753c6d5dce5509837a6280a8fb5d9100
Hei <hei@ubuntu.(none)>
2012-04-11 11:37:57 +0800

    Revert "Add SLQB allocator"
    This reverts commit 14bae3b4db7fee75b4d054ea675c84c8be3eaa16.
    (cherry picked from commit b2a9980f5ba663d42476886358cdd1ad1d79bd0f)
    
    Conflicts:
    
    	init/Kconfig
    	mm/page_alloc.c

--- mm/compaction.c
+++ mm/compaction.c
@@ -85,11 +85,6 @@ static unsigned long isolate_freepages_block(struct zone *zone,
 
 		if (!pfn_valid_within(blockpfn))
 			continue;
-
-		/* Watch for unexpected holes punched in the memmap */
-		if (!memmap_valid_within(blockpfn, page, zone))
-			continue;
-
 		nr_scanned++;
 
 		if (!PageBuddy(page))
@@ -184,11 +179,6 @@ static void isolate_freepages(struct zone *zone,
 		 * pages do not belong to a single zone.
 		 */
 		page = pfn_to_page(pfn);
-
-		/* Watch for unexpected holes punched in the memmap */
-		if (!memmap_valid_within(pfn, page, zone))
-			continue;
-
 		if (page_zone(page) != zone)
 			continue;
 
@@ -355,10 +345,6 @@ static isolate_migrate_t isolate_migratepages(struct zone *zone,
 		 */
 		page = pfn_to_page(low_pfn);
 		if (page_zone(page) != zone)
-			continue;
-
-		/* Watch for unexpected holes punched in the memmap */
-		if (!memmap_valid_within(low_pfn, page, zone))
 			continue;
 
 		/* Skip if free */
