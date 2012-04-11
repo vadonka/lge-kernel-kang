cbffa33994660b750f3234ea5e1f288e4d2c933e
Peter Donka <vadonka@vadonka.com>
2012-04-11 19:59:27 +0200

    Overclock cleanup

--- arch/arm/mach-tegra/odm_kit/star/query/nvodm_query.c
+++ arch/arm/mach-tegra/odm_kit/star/query/nvodm_query.c
@@ -348,7 +348,7 @@ static const NvOdmSdramControllerConfigAdv s_NvOdmStarSmartphoneHynixEmcConfigTa
     {
                   0x20,   /* Rev 2.0 */
 CONFIG_DDR2_SDRAM_FREQ,   /* SDRAM frquency */
-  CONFIG_EMC_CORE_VOLT,   /* EMC core voltage */
+                  1200,   /* EMC core voltage */
                     46,   /* Number of EMC parameters below */
         {
             0x00000012,   /* RC */
@@ -676,7 +676,7 @@ static const NvOdmSdramControllerConfigAdv s_NvOdmStarSmartphoneHynixEmcConfigTa
     {
                   0x20,   /* Rev 2.0 */
 CONFIG_DDR2_SDRAM_FREQ,   /* SDRAM frquency */
-  CONFIG_EMC_CORE_VOLT,   /* EMC core voltage */
+                  1200,   /* EMC core voltage */
                     46,   /* Number of EMC parameters below */
         {
 					  0x00000012,	/* RC */
