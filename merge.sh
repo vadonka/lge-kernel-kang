54e277cb59f3b655fa4ca0c696ca64bb6217ae2f
Peter Donka <vadonka@vadonka.com>
2012-04-12 00:01:55 +0200

    Cherry pick script for linux-kernel stable commits

---
+++ merge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf cherry-pick.sh
+
+echo -e "#!/bin/bash\n" > cherry-pick.sh
+echo -n "Version to merge: "
+
+read fullversion
+version=`echo $fullversion | awk 'BEGIN {FS="."} {print $1}'`
+patchlevel=`echo $fullversion | awk 'BEGIN {FS="."} {print $2}'`
+sublevel=`echo $fullversion | awk 'BEGIN {FS="."} {print $3}'`
+prevsublevel=$(($sublevel-1))
+prevversion=$version.$patchlevel.$prevsublevel
+
+echo -e "# Cherry pick commits from kernel $fullversion\n" >> cherry-pick.sh
+
+for commit in `git log v$prevversion...v$fullversion --pretty=oneline --reverse | awk 'BEGIN {FS=" "} {print $1}'`; do
+	echo "git cherry-pick -x $commit" >> cherry-pick.sh
+done
+
+chmod +x cherry-pick.sh
