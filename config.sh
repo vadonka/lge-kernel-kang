214f1b78bb60859d06c2b3b4b82a4517182aeb30
Peter Donka <vadonka@vadonka.com>
2012-04-16 20:28:36 +0200

    config.sh: support defconfig update

--- config.sh
+++ config.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 source variables
-export cc=/home/android/android/android-toolchain-eabi_4.y/$gccversion/bin/arm-eabi-
 
+while [ -n "$*" ]; do
+flag=$1
+	case "$flag" in
+	"--update-defconfig")
+	if [ -f .config ]; then
+		cp -f .config arch/arm/configs/$defconfig
+		echo "$defconfig is updated succesfully"
+		exit 0
+	else
+		echo ".config file not exist"
+		exit 0
+	fi
+	;;
+	esac
+	shift
+done
+
+export cc=/home/android/android/android-toolchain-eabi_4.y/$gccversion/bin/arm-eabi-
 make ARCH=arm CROSS_COMPILE=$cc menuconfig 2> warn.log
