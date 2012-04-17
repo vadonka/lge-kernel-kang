ab1d05bf48e72949af4b6cb2ebe613a5017a7297
Peter Donka <vadonka@vadonka.com>
2012-04-17 13:08:33 +0200

    Use ccache by default

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #no overclock with stock battery driver
-./compile.sh --gcc 4.7.1
+./compile.sh --gcc 4.7.1 --ccache
 
 #no overclock with DS battery driver
-./compile.sh --ds --gcc 4.7.1
+./compile.sh --ds --gcc 4.7.1 --ccache
 
 #overclock with stock battery driver
-./compile.sh --oc --gcc 4.7.1
+./compile.sh --oc --gcc 4.7.1 --ccache
 
 #overclock with DS battery driver
-./compile.sh --oc --ds --gcc 4.7.1
+./compile.sh --oc --ds --gcc 4.7.1 --ccache
