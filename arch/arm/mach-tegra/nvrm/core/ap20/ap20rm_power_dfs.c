fe2b5e0bdc041d57d4c857bacb6635aa01271e7e
Peter Donka <vadonka@vadonka.com>
2012-04-18 12:09:08 +0200

    Revert "Revert cpu1 activation cleanup"
    This reverts commit 95f04f48dceb2398f98e3206f7d464c689a44e4b.

--- arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_power_dfs.c
+++ arch/arm/mach-tegra/nvrm/core/ap20/ap20rm_power_dfs.c
@@ -691,23 +691,11 @@ NvRmPrivAp20GetPmRequest(
     // - use fixed values if they are defined explicitly, otherwise
     // - set CPU1 OffMax threshold at 2/3 of cpu frequency range,
     //   and half of that frequency as CPU1 OnMin threshold
-    if ((s_Cpu1OffMaxKHz == 0) && (s_Cpu1OnMinKHz == 0))
-    {
         NvRmFreqKHz MaxKHz =
             NvRmPrivGetSocClockLimits(NvRmModuleID_Cpu)->MaxKHz;
 
-#ifdef CONFIG_OTF_CPU1
         s_Cpu1OnMinKHz = NVRM_CPU1_ON_MIN_KHZ;
         s_Cpu1OffMaxKHz = NVRM_CPU1_OFF_MAX_KHZ;
-#else
-        s_Cpu1OnMinKHz = NVRM_CPU1_ON_MIN_KHZ ?
-                         NVRM_CPU1_ON_MIN_KHZ : (MaxKHz / 3);
-        s_Cpu1OffMaxKHz = NVRM_CPU1_OFF_MAX_KHZ ?
-                          NVRM_CPU1_OFF_MAX_KHZ : (2 * MaxKHz / 3);
-        NV_ASSERT(s_Cpu1OnMinKHz < s_Cpu1OffMaxKHz);
-#endif /* OTF_CPU1 */
-
-    }
 
     // Timestamp
     if (s_pTimerUs == NULL)
