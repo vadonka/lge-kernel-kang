2000765312b025f622f4837e8e2606e187bf69f6
Peter Donka <vadonka@vadonka.com>
2012-05-11 12:05:47 +0200

    Simplify auto version script

--- gitversion.sh
+++ gitversion.sh
@@ -1,48 +1,9 @@
 #!/bin/sh
 
-# Tag revisions like this:
-# $ git tag -a -m "Version 0.2" v0.2 HEAD
+#!/bin/bash
+revisioncount=`git log --oneline | wc -l`
+projectversion=`git describe --tags --long`
+cleanversion=${projectversion%%-*}
 
-VF=VERSION
-DEFAULT_VERSION=1.0
-
-LF='
-'
-
-# First see if there is a version file (included in release tarballs),
-# then try git-describe, then default.
-if test -d .git -o -f .git &&
-    VN=$(git describe --abbrev=4 HEAD 2>/dev/null) &&
-    case "$VN" in
-    *$LF*) (exit 1) ;;
-    v[0-9]*)
-        git update-index -q --refresh
-        test -z "$(git diff-index --name-only HEAD --)" ||
-        VN="$VN-mod" ;;
-    esac
-then
-        continue
-    #VN=$(echo "$VN" | sed -e 's/-/./g');
-else
-    VN="$DEFAULT_VERSION"
-fi
-
-VN=$(expr "$VN" : v*'\(.*\)')
-
-# Show the version to the user via stderr
-echo >&2 "version: $VN"
-
-# Parse the existing VERSION-FILE 
-if test -r $VF
-then
-    VC=$(sed -e 's/^version: //' <$VF)
-else
-    VC=unset
-fi
-
-# If version has changed, update VERSION-FILE
-test "$VN" = "$VC" || {
-    echo "version: $VN" >$VF
-    echo >&2 "($VF updated)"
-}
-
+echo "$projectversion-$revisioncount" > VERSION
+#echo "$cleanversion.$revisioncount" > VERSION
