e1b284f485fd5449b026397dc86579f888c1c1cc
Peter Donka <vadonka@vadonka.com>
2012-05-21 20:59:12 +0200

    Adjust io sched for nand

--- block/vr-iosched.c
+++ block/vr-iosched.c
@@ -41,7 +41,7 @@ BACKWARD,
 
 static const int sync_expire = HZ / 2; /* max time before a sync is submitted. */
 static const int async_expire = 5 * HZ; /* ditto for async, these limits are SOFT! */
-static const int fifo_batch = 8;
+static const int fifo_batch = 2;
 static const int rev_penalty = 1; /* penalty for reversing head direction */
 
 struct vr_data {
