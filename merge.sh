6fd149b3cf5f6cbbaa35110c45af17d169fd8a1b
Peter Donka <vadonka@vadonka.com>
2012-05-22 13:20:18 +0200

    Update linux stable merge script

--- merge.sh
+++ merge.sh
@@ -14,8 +14,13 @@ prevversion=$version.$patchlevel.$prevsublevel
 
 echo -e "# Cherry pick commits from kernel $fullversion\n" >> linux-stable-cherry-pick.sh
 
+cc=1
 for commit in `git log v$prevversion...v$fullversion --pretty=oneline --reverse | awk 'BEGIN {FS=" "} {print $1}'`; do
-	echo "git cherry-pick -x $commit" >> linux-stable-cherry-pick.sh
+	echo "c$cc=\`git log --format=full | grep -c $commit\`" >> linux-stable-cherry-pick.sh
+	echo "if [ \"\$c$cc\" = \"0\" ]; then" >> linux-stable-cherry-pick.sh
+	echo -e "\tgit cherry-pick -x $commit" >> linux-stable-cherry-pick.sh
+	echo "fi" >> linux-stable-cherry-pick.sh
+	cc=$(($cc+1))
 done
 
 chmod +x linux-stable-cherry-pick.sh
